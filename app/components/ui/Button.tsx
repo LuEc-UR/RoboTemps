e2656aa59dda958309cfba94c20287f25f798a9e
Ilaria Delfanti <PC29@cdssrl.local>
2025-12-09 12:20:20 +0100

    Update: import&bug

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { cn } from "@/lib/utils";
+import { cn } from "../lib/utils";
 
 interface ButtonProps {
   href?: string;
